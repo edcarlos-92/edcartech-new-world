1effcbefc93eed56bcf5375fe19b1abbf646bddd
edcarlos-92 <edcartech@gmail.com>
2025-10-07 21:56:42 -0700

    fix: remove problematic global event listener from clickListerner.tsx

--- lib/hooks/clickListerner.tsx
+++ lib/hooks/clickListerner.tsx
@@ -1,18 +1,7 @@
 import { useCallback, useState } from "react";
 import { useEventListener } from "./useEventListener";
 
-function onClick() {
-    console.log("scroll!");
-
-    //let contexThemeChange = window.localStorage.getItem('theme');
-    //setTheme(contexThemeChange);
-    return window.localStorage.getItem('theme');
-
-  }
-  
-  window.addEventListener("click", onClick);
-  
-  export default function click(props:any) {
+export default function click(props:any) {
 //     let contexTheme = window && window.localStorage.getItem('theme');
 //     const [getTheme,setTheme] = useState(contexTheme);
 //     const handler = useCallback(() => {
