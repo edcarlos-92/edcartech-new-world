c30e9c21447c144c7789370e6a7c6cbfcf5c3363
edcarlos-92 <edcartech@gmail.com>
2025-10-07 22:05:23 -0700

    fix: move external scripts to body for proper loading order

--- lib/hooks/clickListerner.tsx
+++ lib/hooks/clickListerner.tsx
@@ -1,18 +1,18 @@
 import { useCallback, useState } from "react";
 import { useEventListener } from "./useEventListener";
 
-export default function click(props:any) {
-//     let contexTheme = window && window.localStorage.getItem('theme');
-//     const [getTheme,setTheme] = useState(contexTheme);
-//     const handler = useCallback(() => {
-//     let contexThemeChange = window.localStorage.getItem('theme');
-//       setTheme(contexThemeChange);
-//       console.log(`contexTheme`,getTheme);
-//     },
-//     [getTheme]
-//     );
-//   // // Add event listener using the hook
-//    useEventListener('click', handler);
+export default function click(props: any) {
+  //     let contexTheme = window && window.localStorage.getItem('theme');
+  //     const [getTheme,setTheme] = useState(contexTheme);
+  //     const handler = useCallback(() => {
+  //     let contexThemeChange = window.localStorage.getItem('theme');
+  //       setTheme(contexThemeChange);
+  //       console.log(`contexTheme`,getTheme);
+  //     },
+  //     [getTheme]
+  //     );
+  //   // // Add event listener using the hook
+  //    useEventListener('click', handler);
 
-    return null;
-  }
+  return null;
+}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,12 +29,7 @@ export default class MyDocument extends Document {
           {/* <script>{hljs.initHighlightingOnLoad();}</script> */}
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
-    <script src='/assets/js/portfolio.js'></script>
 
 
 
@@ -49,6 +44,11 @@ export default class MyDocument extends Document {
           <Main />
           <div id='root' />
           <NextScript />
+          <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+          <script src='/assets/js/portfolio.js'></script>
         </body>
       </Html>
       
