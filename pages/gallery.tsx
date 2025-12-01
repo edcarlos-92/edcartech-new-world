e3a2b05d2f3e39170f3f343eba1e11becc347d8f
edcarlos-92 <edcartech@gmail.com>
2025-12-01 04:08:52 -0800

    Redirect Page in case of Gallery not loading

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,10 +1,24 @@
-import { Fragment } from 'react'
+import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import MainMenu from '../components/MainMenu'
 import Footer from '../components/Footer'
 import { themeConfig } from '../lib/themeConfig'
 
 export default function GalleryPage() {
+   const [isGalleryLoaded, setIsGalleryLoaded] = useState(false)
+   const galleryUrl = 'https://www.photo-pick.com/online/8a4qSf09.link'
+
+   // If the embedded gallery hasn't loaded after 5 seconds, redirect visitor
+   useEffect(() => {
+      const timer = setTimeout(() => {
+         if (!isGalleryLoaded && typeof window !== 'undefined') {
+            window.location.href = galleryUrl
+         }
+      }, 5000)
+
+      return () => clearTimeout(timer)
+   }, [isGalleryLoaded, galleryUrl])
+
    return (
       <div className={themeConfig.backgrounds.main}>
          <Head>
@@ -39,7 +53,7 @@ export default function GalleryPage() {
                         </p>
                      </div>
                      <a
-                        href="https://www.photo-pick.com/online/8a4qSf09.link"
+                        href={galleryUrl}
                         target="_blank"
                         rel="noreferrer"
                         className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-colors duration-200 attention-button hover:animate-none"
@@ -47,32 +61,35 @@ export default function GalleryPage() {
                         <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
                         </svg>
-                        Click here to Open in New Tab in case of slow loading Frame below
+                        Please Click here to Open in New Tab in case of slow loading Frame below
                      </a>
                   </div>
 
                   <div className="relative" style={{ height: '80vh' }}>
                      {/* Try object tag first - sometimes works better than iframe */}
                      <object
-                        data="https://www.photo-pick.com/online/8a4qSf09.link"
+                        data={galleryUrl}
                         type="text/html"
                         className="w-full h-full border-0"
                         title="Project Gallery"
+                        onLoad={() => setIsGalleryLoaded(true)}
                      >
                         {/* Fallback to embed tag */}
                         <embed
-                           src="https://www.photo-pick.com/online/8a4qSf09.link"
+                           src={galleryUrl}
                            type="text/html"
                            className="w-full h-full border-0"
                            title="Project Gallery"
+                           onLoad={() => setIsGalleryLoaded(true)}
                         />
                         {/* Final fallback to iframe */}
                         <iframe
-                           src="https://www.photo-pick.com/online/8a4qSf09.link"
+                           src={galleryUrl}
                            className="w-full h-full border-0"
                            title="Project Gallery"
                            allowFullScreen
                            loading="lazy"
+                           onLoad={() => setIsGalleryLoaded(true)}
                         />
                      </object>
                   </div>
@@ -83,7 +100,7 @@ export default function GalleryPage() {
                   <p className="text-gray-600 dark:text-gray-400">
                      Gallery not loading?
                      <a
-                        href="https://www.photo-pick.com/online/8a4qSf09.link"
+                        href={galleryUrl}
                         target="_blank"
                         rel="noreferrer"
                         className="text-blue-600 dark:text-blue-400 hover:underline ml-1"
